e244825369685cb67dfe9080d69cfff7917bf571
hayalebrach <109691157+hayalebrach@users.noreply.github.com>
2022-08-08 11:09:37 +0300

    שיואוו

--- src/components/GuessNavBar.js
+++ src/components/GuessNavBar.js
@@ -2,9 +2,6 @@ import{Link} from "react-router-dom";
 import "./App.css";
 export default function GuessNavBar(){
     return <nav className="navbar navbar-default" >
-    
-   <Link to="login" className="navbar-brand">התחברות</Link>
-   <Link to="signIn" className="navbar-brand" >הרשמה</Link>
    <Link to="home" className="navbar-brand"> לבריכות</Link>
    <Link to="ManagerNavBar" className="navbar-brand">לבריכה שלי!!!</Link>
    <Link to="AddPool" className="navbar-brand">הוספת בריכה</Link>
