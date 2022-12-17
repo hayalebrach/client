2dbe2447683af733d68e864e4f08f9067fae22cd
hayalebrach <109691157+hayalebrach@users.noreply.github.com>
2022-12-17 21:27:51 +0200

    עמוד מנהל-כרטיסים
    אפשרות לבחירה:עדכון,הוספה,מחיקה
    בצורה יפה

--- src/components/ManagerNavBar.js
+++ src/components/ManagerNavBar.js
@@ -1,9 +1,13 @@
-import{ React}  from "react";
+import * as React from 'react';
 import{Link} from "react-router-dom";
+
 import "./App.css";
+import"./ManagerNavBar.css";
+
 
 export default function ManagerNavBar(){
-    return <nav  className="navbar navbar-default">
+ 
+    return (<><nav  className="navbar navbar-default">
 
     
         <Link to="AllUsers" className="navbar-brand">צפייה בלקוחות  </Link>
@@ -12,8 +16,25 @@ export default function ManagerNavBar(){
         <Link to="addSale" className="navbar-brand">להוספת מבצעים </Link>
         {/* <Link to="courses" className="navbar-brand">לקורסים</Link> */}
         <Link to="AddDetailsSchedule" className="navbar-brand">לצפיה בזמני בריכה</Link>
+        
+      
+
+
+
+
+
+  
+
+
+    
+ 
+
+
+
+        
 
 
     
     </nav>
+    </>)
 }
--- src/components/UpdateCard/AllCardsToPool.js
+++ src/components/UpdateCard/AllCardsToPool.js
@@ -1,8 +1,14 @@
+import * as React from 'react';
 import {useEffect, useState} from "react";
 import {useSelector,useDispatch,shallowEqual}from "react-redux";
 import {useNavigate} from "react-router";
 import {getAllCardByIdPool,getById,DeletCard}from "../../store/Actions/Card";
-
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import "./AllCardsToPool.css";
 export default function AllCardsToPool(){
   const {currentPool,CardsArr,currentCard} = useSelector(state => ({
     currentPool: state.currentPool,
@@ -16,6 +22,12 @@ export default function AllCardsToPool(){
   const nav=useNavigate();
 
   const dispatch=useDispatch();
+  const [act, setAct] = React.useState('');
+  const[a,Seta]=useState() ;
+  const handleChange = (event) => {
+    setAct(event.target.value);
+    Seta(event.target.value);
+  };
 
 const update=(Id)=>{
   console.log(" ה' רק אתה יכול לעזור לי בזה");
@@ -32,14 +44,44 @@ const Delet=(Id)=>{
   return(
     <>
     <h1>כל הכרטיסים</h1>
+    
+    
+   
+    <Box id="css-0">
+      <FormControl id="css-1nrlq1o-MuiFormControl-root">
+        <InputLabel id="demo-simple-select-label">אפשרויות</InputLabel>
+        <Select 
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={act}
+          label="אפשרויות"
+          onChange={handleChange}
+        >
+          <MenuItem value={10} id="css-hfutr2-MuiSvgIcon-root-MuiSelect-icon" onClick={()=>nav("/AddDetailsCard/true")}>הוספה</MenuItem>
+          <MenuItem value={20} id="css-hfutr2-MuiSvgIcon-root-MuiSelect-icon">עדכון</MenuItem>
+          <MenuItem value={30} id="css-hfutr2-MuiSvgIcon-root-MuiSelect-icon" >מחיקה</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+
+
     <ul>
         {
             CardsArr.map(CardsArr=><><br/> <li className="li" key={CardsArr.Id}><h3>id: {CardsArr.Id}</h3><br/> מחיר:{CardsArr.Price}<br/> כמות בניסות:{CardsArr.EntersAmount}<br/>
-            <input type="button" value="עדכון" onClick={()=>update(CardsArr.Id)}/><br/> <input type="button" value="מחיקה" onClick={()=>Delet(CardsArr.Id)}/></li></> )
+            {a==20?<input type="button" value="עדכון" onClick={()=>update(CardsArr.Id)}/>:null}
+            {a==30?<input type="button" value="מחיקה" onClick={()=>Delet(CardsArr.Id)}/>:null}
+            
+            <br/> </li></> )
         }
     </ul>
-    <input type="button" value="הוספת כרטיס" onClick={()=>nav("/AddDetailsCard/true")}/>
     <input type="button" value="חזרה לעמוד הבית" onClick={()=>nav("/ManagerNavBar")}/>
+
+
+
+
+
+
+
     </>
   )
 }
